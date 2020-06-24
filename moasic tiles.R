2e2d9b94a3b1252b5efd3aba24ebc633369de853
Emily Ury <ury.emily@gmail.com>
2020-06-24 17:03:30 -0400

    fix plot color

--- moasic tiles.R
+++ moasic tiles.R
@@ -46,5 +46,5 @@ tile11 <- raster("composite2017tile11.tif")
 
 m.all <- mosaic(tile1, tile2, tile3, tile4, tile9, tile5, tile6, tile7, tile7a, tile8, tile10,
                 tile11, fun = mean)
-plot(m.all, col = "green")
+plot(m.all, col = "darkgreen")
 plot(m1, col = "blue", add = T)
