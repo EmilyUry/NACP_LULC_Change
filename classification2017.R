63bb136e84c4dea3f548513a83a25d11a440ec5a
Emily Ury <ury.emily@gmail.com>
2020-06-25 15:23:44 -0400

    made classified map for 2017

--- classification2017.R
+++ classification2017.R
@@ -7,15 +7,16 @@ library(raster)
 library(rgdal)
 library(randomForest)
 library(rsample)
+library(sf)
 
-raster <- raster("region1.tif")
-plot(raster, layer = 1)
+raster <- raster("bothwatersheds.tif")
+plot(raster)
 
-vps <- read.csv("sample2017vps.csv", head = T)
+vps <- read.csv("2017vpsMini.csv", head = T)
 
 points(vps$x, vps$y, pch = "+", cex = 0.3)
 
-stack <- brick("region1.tif")
+stack <- brick("bothwatersheds.tif")
 names(stack)
 
 
@@ -24,11 +25,11 @@ names(stack)
 ####
 # make the validation points into a spatial points dataframe
 crs <- crs(raster)
-vps.sp <- SpatialPointsDataFrame(vps[,2:3], vps, proj4string = crs)
+vps.sp <- SpatialPointsDataFrame(vps[,1:2], vps, proj4string = crs)
 vps.sp
-classes <- rasterize(vps.sp, stack, field = 'class')
+classes <- rasterize(vps.sp, raster, field = 'Class')  # mak
 
-cols <- c("palegreen4", "aquamarine3", "lightgoldenrod1", "darkgreen", "brown3", "burlywood4", "gray80")
+cols <- c("palegreen4", "aquamarine3", "lightgoldenrod1", "brown3", "burlywood4", "gray80")
 
 # mask raster brick to training points
 covmask <- mask(stack, classes)
@@ -36,17 +37,18 @@ names(classes) <- "class"
 trainingbrick <- addLayer(covmask,classes)
 #plot(trainingbrick)
 valuetable <- getValues(trainingbrick)
-valuetable <- na.omit(valuetable)
-valuetable <- as.data.frame(valuetable)
+rm(trainingbrick)
+rm(classes)
+valuetable <- na.omit(valuetable)       ### if this doesnt work try memory.limit(size =65176*2) or run line 42 before line 41
+valuetable <- as.data.frame(valuetable) 
 #head(valuetable)
-valuetable$class <- factor(valuetable$class, levels = c(1:7))
-
+valuetable$class <- factor(valuetable$class, levels = c(1:6))
 
 
 
 ##### make the RF model
 set.seed(123)
-valuetable$random = sample(2, nrow(valuetable), replace=TRUE, prob=c(0.7,0.3))
+valuetable$random = sample(2, nrow(valuetable), replace=TRUE, prob=c(0.8,0.2))
 trainData = valuetable[valuetable$random==1,]
 testData = valuetable[valuetable$random==2,]
 
@@ -59,14 +61,11 @@ prediction <- predict(modelRF, newdata=testData)
 #table(prediction, testData$class)
 cm <- table(prediction, testData$class)
 accuracy <- (sum(diag(cm)))/sum(cm)
-accuracy  ## 70.1 (seed:123)
-
-
+accuracy  ## 82.1 (seed:123)
 
 
 ##  overall model
 modelRF <- randomForest(x=valuetable[,c(1:12)], y=valuetable$class, importance = TRUE)
-
 
 
 ### Map prediction
@@ -76,6 +75,8 @@ names(valuetable) ## these must match
 map17 <- predict(stack, model=modelRF, na.rm=TRUE)
 plot(map17, col = cols, legend = FALSE)
 
+
+writeRaster(map17, filename="classified2017", format="GTiff", overwrite=TRUE)
 
 
 
