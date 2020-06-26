e73c36b21d891cc01103c4ff3337311d950fe684
Emily Ury <ury.emily@gmail.com>
2020-06-25 22:55:37 -0400

    water vector not working

--- clip map.R
+++ clip map.R
@@ -9,4 +9,12 @@ classmap17 <- raster("classified2017.tif")
 #### cut map to watershed and cut out water
 C301 <- st_read("CP_301export.shp")
 C302 <- st_read("CP_302export.shp")
-water <- st_read("cp")
+water <- st_read("water_vector.shp")
+
+
+cols <- c("palegreen4", "aquamarine3", "lightgoldenrod1", "brown3", "burlywood4", "gray50")
+
+plot(classmap17, col = cols)
+plot(C301, add = T)
+plot(C302, add = T)
+plot(water, add = T, col = "blue")
