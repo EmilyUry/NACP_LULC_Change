6dc54fba39d9c2b028dac0748c221ad8a69a460b
Emily Ury <ury.emily@gmail.com>
2020-07-01 13:33:05 -0400

    classify 1996 layer using 2010 RF model

--- classification1996_select.R
+++ classification1996_select.R
@@ -13,7 +13,32 @@ cols <- c("palegreen4", "aquamarine3", "lightgoldenrod1", "brown3", "burlywood4"
 ### Mosaic regions
 #____Do this first, only once
 
-# R1 <- brick("composite2010_R1.tif")
-# R2 <- brick("composite2010_R2.tif")
-# m.all <- mosaic(R1, R2, fun = mean)
-# writeRaster(m.all, "NL_trim_2010.tif", overwrite = T)
+R1 <- brick("composite1996_R1.tif")
+R2 <- brick("composite1996_R2.tif")
+m.all <- mosaic(R1, R2, fun = mean)
+writeRaster(m.all, "NL_trim_1996.tif", overwrite = T)
+
+
+
+raster <- raster("NL_trim_1996.tif")
+plot(raster)
+stack <- brick("NL_trim_1996.tif")
+names(stack)
+names(stack) <- c("NL_trim_2010.1",  "NL_trim_2010.2",  "NL_trim_2010.3",  "NL_trim_2010.4",  "NL_trim_2010.5",
+                  "NL_trim_2010.6", "NL_trim_2010.7",  "NL_trim_2010.8",  "NL_trim_2010.9",  "NL_trim_2010.10", 
+                  "NL_trim_2010.11", "NL_trim_2010.12")
+
+### must run the whole 2010 script to generate modelRF (~10 mins)
+
+
+### Map prediction
+
+
+names(valuetable) ## these must match names(stack)
+map96 <- predict(stack, model=modelRF, na.rm=TRUE)
+plot(map96, col = cols, legend = FALSE)
+
+## if the plot looks good, write out the layer:
+
+writeRaster(map96, filename="classified1996_select", format="GTiff", overwrite=TRUE)
+
